f26087ed918b1b42daf87204a00fdfe7d7378a27
arysuryo17 <56426548+arysuryo17@users.noreply.github.com>
2025-08-16 10:48:34 +0000

    Add Calculator Project

--- src/Ch02/02_05e/CodeDemo.cpp
+++ src/Ch02/02_05e/CodeDemo.cpp
@@ -24,3 +24,44 @@ int main(){
     std::cout << std::endl << std::endl;
     return 0;
 }
+
+
+/*#include <iostream>
+using namespace std;
+
+int main() {
+    char op;
+    float num1, num2;
+
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+    cout << "Enter number one: ";
+    cin >> num1;
+    cout << "Enter number two: "; 
+    cin >> num2;
+
+    switch(op) {
+        case '+':
+        cout <<"The result is ";
+            cout << num1 + num2;
+            break;
+        case '-':
+        cout <<"The result is ";
+            cout << num1 - num2;
+            break;
+        case '*':
+        cout <<"The result is ";
+            cout << num1 * num2;
+            break;
+        case '/':
+        cout <<"The result is ";
+            if(num2 != 0)
+                cout << num1 / num2;
+            else
+                cout << "Division by zero!";
+            break;
+        default:
+            cout << "Invalid operator";
+    }
+    return 0;
+}*/
